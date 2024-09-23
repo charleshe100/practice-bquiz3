c84ad1322c76f8e03d3c79c6c7a7fa2860596443
charles100 <brs-68@yahoo.com.tw>
2024-09-23 23:49:19 +0800

    製作前台預告片展示版面

--- front/main.php
+++ front/main.php
@@ -1,22 +1,37 @@
 <div class="half" style="vertical-align:top;">
-  <h1>預告片介紹</h1>
-  <div class="rb tab" style="width:95%;">
-    <div id="abgne-block-20111227">
-      <ul class="lists">
-      </ul>
-      <ul class="controls">
-      </ul>
+    <h1>預告片介紹</h1>
+    <div class="rb tab" style="width:95%;">
+        <!--海報區-->
+        <div class="lists">
+            <?php
+            $posters=$Poster->all(['sh'=>1]," order by rank");
+            foreach($posters as $idx => $poster){
+            ?>
+            <div class="item" data-ani="<?=$poster['ani'];?>">
+                <div><img src="./img/<?=$poster['img'];?>" alt=""></div>
+                <div><?=$poster['name'];?></div>
+            </div>
+            <?php
+            }
+            ?>
+        </div>
+
+        <!--按鈕區-->
+        <div class="controls">
+            <div class="left"></div>
+            <div class="btns">
+            <?php
+            foreach($posters as $idx =>$poster ){
+            ?>
+            <div class="btn">
+                <div><img src="./img/<?=$poster['img'];?>"></div>
+                <div><?=$poster['name'];?></div>
+            </div>
+            <?php 
+            }
+            ?>
+            </div>
+            <div class="right"></div>
+        </div>
     </div>
-  </div>
-</div>
-<div class="half">
-  <h1>院線片清單</h1>
-  <div class="rb tab" style="width:95%;">
-    <table>
-      <tbody>
-        <tr> </tr>
-      </tbody>
-    </table>
-    <div class="ct"> </div>
-  </div>
 </div>
