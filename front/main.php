dece3bad1477b339784381e73d7eca005e8b8c5b
charles100 <brs-68@yahoo.com.tw>
2024-09-24 23:55:13 +0800

    製作前台預告片海報自動輪播功能

--- front/main.php
+++ front/main.php
@@ -35,3 +35,41 @@
         </div>
     </div>
 </div>
+<script>
+$(".item").eq(0).show();
+let total=$(".btn").length
+let now=0;
+let next=0;
+let timer=setInterval(()=>{slide()},3000)
+
+function slide(n){
+    let ani=$(".item").eq(now).data("ani");
+    if(typeof(n)=='undefined'){
+        next=now+1;
+        if(next>=total){
+            next=0;
+        }
+    }else{
+        next=n;
+    }
+
+    switch(ani){
+        case 1:        
+            $(".item").eq(now).fadeOut(1000,function(){            
+                $(".item").eq(next).fadeIn(1000);
+            });
+        break;
+        case 2:        
+            $(".item").eq(now).hide(1000,function(){
+                $(".item").eq(next).show(1000);
+            });
+        break;
+        case 3:        
+            $(".item").eq(now).slideUp(1000,function(){
+                $(".item").eq(next).slideDown(1000);
+            });
+        break;
+    }    
+    now=next;
+}
+</script>
