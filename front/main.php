ac0ee058148a4f6daebdc55cf35755298376b33b
charles100 <brs-68@yahoo.com.tw>
2024-09-25 23:59:47 +0800

    製作前台預告片海報左右移動功能

--- front/main.php
+++ front/main.php
@@ -19,18 +19,18 @@
         <!--按鈕區-->
         <div class="controls">
             <div class="left"></div>
-            <div class="btns">
-            <?php
-            foreach($posters as $idx =>$poster ){
-            ?>
-            <div class="btn">
-                <div><img src="./img/<?=$poster['img'];?>"></div>
-                <div><?=$poster['name'];?></div>
-            </div>
-            <?php 
-            }
-            ?>
-            </div>
+                <div class="btns">
+                <?php
+                foreach($posters as $idx =>$poster ){
+                ?>
+                <div class="btn">
+                    <div><img src="./img/<?=$poster['img'];?>"></div>
+                    <div><?=$poster['name'];?></div>
+                </div>
+                <?php 
+                }
+                ?>
+                </div>
             <div class="right"></div>
         </div>
     </div>
@@ -72,4 +72,38 @@ function slide(n){
     }    
     now=next;
 }
+
+// 控制中間的預告片可左右移動
+let p=0;
+$(".left,.right").on("click",function(){
+    let arrow=$(this).attr('class')
+    switch(arrow){
+        case "right":
+            if(p+1<=(total-4)){
+                p=p+ 1;
+            }
+        break;
+        case "left":
+            if(p-1>=0){
+                p=p-1;
+            }
+        break;
+    }
+    $(".btn").animate({right:90*p})
+})
+
+$(".btn").on('click',function(){
+    let idx=$(this).index()
+    slide(idx);
+})
+
+// 鼠標移入中斷輪播，移出再自動輪播
+$(".btns").hover(
+    function (){
+        clearInterval(timer)
+    },
+    function(){
+        timer=setInterval(()=>{slide()},3000)
+    }
+)
 </script>
